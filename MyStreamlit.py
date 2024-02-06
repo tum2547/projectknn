f63a014b3c116d4c161a5287addfbeba88bee3f0
tum2547 <156771696+tum2547@users.noreply.github.com>
2024-02-06 14:55:47 +0700

    Update MyStreamlit.py

--- MyStreamlit.py
+++ MyStreamlit.py
@@ -3,7 +3,7 @@ import streamlit as st
 st.image('./pic/421525287_757613326296074_2482593574861804964_n.jpg')
 col1, col2 = st.columns(2)
 with col1:
-  st.header('ไก้รุ่ง เฮงพระพรหม')
+  st.header('ธัญสิทธิ์ เดชสิทธิ์ธาวิน')
 with col2:
   st.subheader('สาขาวิชาวิทยาการข้อมูล')
   st.text('คณะวิทยาศาสตร์และเทคโนโลยี')
